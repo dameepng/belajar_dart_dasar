4336e0ed9a8d549b1bbbf5b13d37647644baaa65
dameepng <ahmaddamanhuri45@gmail.com>
2026-04-21 15:06:57 +0700

    add: for

---
+++ bin/for.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  // for(;;){
+  //   print("Infinite Loop");
+  // }
+
+  // var counter = 1;
+  //
+  // for (; counter <= 10; counter) {
+  //   print("Perulangan ke $counter");
+  //
+  //   counter++;
+  // }
+
+  for (var counter = 1; counter <= 10; counter++) {
+    print("Perulangan ke $counter");
+  }
+
+}
