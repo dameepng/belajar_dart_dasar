f44afd4b34a5e4ccf23b8d6e52ed63dd8db9ad80
dameepng <ahmaddamanhuri45@gmail.com>
2026-04-21 13:03:17 +0700

    add: ternary_operator

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  var nilai = 75;
+  var ucapan = nilai >= 75 ? "Selamat Anda Lulus" : "Maaf Anda Tidak Lulus";
+
+  // if (nilai >= 75) {
+  //   ucapan = "Selamat Anda Lulus";
+  // } else {
+  //   ucapan = "Maaf Anda Tidak Lulus";
+  // }
+
+  print(ucapan);
+
+}
