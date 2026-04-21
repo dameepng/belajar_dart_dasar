0906e5a9fe94e6dd84b97d2053bb2745f6c4d684
dameepng <ahmaddamanhuri45@gmail.com>
2026-04-21 11:32:07 +0700

    add: list

---
+++ bin/list.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  List<int> listInt = [];
+
+  var listString = <String>[];
+
+  print(listInt);
+  print(listString);
+
+  var names = <String>[
+    'Ahmad',
+    'Daman',
+    'Huri'
+  ];
+
+  // names.add('ahmad');
+  // names.add('daman');
+  // names.add('huri');
+
+  print(names);
+  print(names[0]);
+  print(names[1]);
+  print(names[2]);
+  print(names.length);
+
+}
